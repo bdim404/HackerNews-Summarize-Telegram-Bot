25b461ed03b2425409c9b8633552ebaa9717bd42
bdim <i@bdim.moe>
2023-10-24 20:11:08 +0800

    Update hackernewSummarizeBot.py
    add verify

--- hackernewSummarizeBot.py
+++ hackernewSummarizeBot.py
@@ -16,6 +16,11 @@ from telegram.ext import (
 )
 import re
 from dotenv import load_dotenv
+<<<<<<< Updated upstream
+=======
+import logging
+import urllib.parse
+>>>>>>> Stashed changes
 
 # 从 .env 文件加载配置
 load_dotenv()
@@ -59,6 +64,23 @@ async def handle_message(update, context: ContextTypes.DEFAULT_TYPE) -> None:
     text = update.message.text
     links_match = re.search(r"Link:\s+(https://\S+)", text)
     comments_match = re.search(r"Comments:\s+(https://\S+)", text)
+
+#验证链接是否来自Hacker News
+    if links_match:
+        links = links_match.group(1)
+        if not is_valid_link(links):
+            await update.message.reply_text("警告！你发送了包含错误的链接的消息，请转发正确的消息。")
+            user_id = str(update.message.from_user.id)
+            logging.info(f"User ID: {user_id} send a wrong links")
+            return
+
+    if comments_match:
+        comments = comments_match.group(1)
+        if not is_valid_link(comments):
+            await update.message.reply_text("警告！你发送了包含错误的链接的消息，请转发正确的消息。")
+            user_id = str(update.message.from_user.id)
+            logging.info(f"User ID: {user_id} send a wrong links")
+            return
 
     if links_match:
         links = links_match.group(1)
@@ -117,7 +139,20 @@ def truncate_text(text, max_length):
     else:
         # 从文本的末尾开始删除字符，直到满足最大长度
         return text[:-(len(text) - max_length)]
+<<<<<<< Updated upstream
     
+=======
+
+import urllib.parse
+
+#验证域名是否来自redhacker.news
+def is_valid_link(link):
+    parsed_url = urllib.parse.urlparse(link)
+    return parsed_url.netloc == "readhacker.news"
+
+
+
+>>>>>>> Stashed changes
 def main():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()  # 使用从 .env 文件中获取的 Telegram Bot Token
     app.add_handler(CommandHandler("start", start))
